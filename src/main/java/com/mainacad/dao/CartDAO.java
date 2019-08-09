5968930b03a9bd4f8927335fa3eceec475545e2b
stersk <sters@ua.fm>
2019-08-09 17:23:42 +0300

    CartDAO realization and test

--- src/main/java/com/mainacad/dao/CartDAO.java
+++ src/main/java/com/mainacad/dao/CartDAO.java
@@ -1,28 +1,26 @@
 package com.mainacad.dao;
 
 import com.mainacad.model.Cart;
-import com.mainacad.model.User;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class CartDAO {
+    private static Logger logger = Logger.getLogger(OrderDAO.class.getName());
 
     /**
      * @param cart must be always open (closed = false)
      * @return stored cart with id
      */
-    public static Cart create(Cart cart){
+    public static Cart create(Cart cart) {
         String sql = "INSERT INTO carts(creation_time, closed, user_id) VALUES(?,?,?)";
         String sequenceSql = "SELECT currval(pg_get_serial_sequence('carts','id'))";
 
         try (Connection connection = ConnectionToDB.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql);
-             PreparedStatement seqStatement = connection.prepareStatement(sequenceSql)
-        ){
+             Statement seqStatement = connection.createStatement()) {
 
             preparedStatement.setLong(1, cart.getCreationTime());
             preparedStatement.setBoolean(2, cart.getClosed());
@@ -30,7 +28,7 @@ public class CartDAO {
 
             preparedStatement.executeUpdate();
 
-            ResultSet resultSet = seqStatement.executeQuery();
+            ResultSet resultSet = seqStatement.executeQuery(sequenceSql);
             while (resultSet.next()) {
                 Integer id = resultSet.getInt(1);
                 cart.setId(id);
@@ -38,18 +36,35 @@ public class CartDAO {
                 return cart;
             }
         } catch (SQLException e){
-            e.printStackTrace();
+            logger.severe(e.getMessage());
         }
 
         return null;
     }
 
-    public static Cart update(Cart cart){
+    public static Cart update(Cart cart) {
+        String statement = "UPDATE carts SET creation_time=?, closed=?, user_id=? WHERE id=?";
+
+        try (Connection connection = ConnectionToDB.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(statement)) {
+
+            preparedStatement.setLong(1, cart.getCreationTime());
+            preparedStatement.setBoolean(2, cart.getClosed());
+            preparedStatement.setInt(3, cart.getUserId());
+            preparedStatement.setInt(4, cart.getId());
+
+            preparedStatement.executeUpdate();
+
+            return cart;
+
+        } catch (SQLException e) {
+            logger.severe(e.getMessage());
+        }
 
         return null;
     }
 
-    public static Cart findById(Integer id){
+    public static Cart findById(Integer id) {
         String statement = "SELECT * FROM carts WHERE id=?";
 
         try (Connection connection = ConnectionToDB.getConnection();
@@ -60,29 +75,20 @@ public class CartDAO {
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
-                Cart cart = new Cart();
-
-                cart.setId(resultSet.getInt("id"));
-                cart.setCreationTime(resultSet.getLong("creation_time"));
-                cart.setClosed(resultSet.getBoolean("closed"));
-                cart.setUserId(resultSet.getInt("user_id"));
-
-                return cart;
+                return getCartFromResultSetItem(resultSet);
             }
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.severe(e.getMessage());
         }
-        return null;
-    }
-
-    public static List<Cart> findByUser(User user){
 
         return null;
     }
 
-    public static Cart findOpenCartByUser(Integer userId){
-        String statement = "SELECT * FROM carts WHERE closed=0 AND user_id=?";
+    public static List<Cart> findByUser(Integer userId) {
+        List<Cart> cartList = new ArrayList<>();
+
+        String statement = "SELECT * FROM carts WHERE user_id=?";
 
         try (Connection connection = ConnectionToDB.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(statement)) {
@@ -92,19 +98,60 @@ public class CartDAO {
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
-                Cart cart = new Cart();
-
-                cart.setId(resultSet.getInt("id"));
-                cart.setCreationTime(resultSet.getLong("creation_time"));
-                cart.setClosed(resultSet.getBoolean("closed"));
-                cart.setUserId(resultSet.getInt("user_id"));
-
-                return cart;
+                cartList.add(getCartFromResultSetItem(resultSet));
             }
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.severe(e.getMessage());
         }
+
+        return cartList;
+    }
+
+    public static Cart findOpenCartByUser(Integer userId){
+        String statement = "SELECT * FROM carts WHERE closed='0' AND user_id=?";
+
+        try (Connection connection = ConnectionToDB.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(statement)) {
+
+            //preparedStatement.setBoolean(1, false);
+            preparedStatement.setInt(1, userId);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                return getCartFromResultSetItem(resultSet);
+            }
+
+        } catch (SQLException e) {
+            logger.severe(e.getMessage());
+        }
+
         return null;
+    }
+
+    public static void delete(Integer cartId){
+        String statement = "DELETE FROM carts WHERE id=?";
+
+        try (Connection connection = ConnectionToDB.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(statement)) {
+
+            preparedStatement.setInt(1, cartId);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            logger.severe(e.getMessage());
+        }
+    }
+
+    private static Cart getCartFromResultSetItem(ResultSet resultSet) throws SQLException {
+        Cart cart = new Cart();
+
+        cart.setId(resultSet.getInt("id"));
+        cart.setCreationTime(resultSet.getLong("creation_time"));
+        cart.setClosed(resultSet.getBoolean("closed"));
+        cart.setUserId(resultSet.getInt("user_id"));
+
+        return cart;
     }
 }
---
+++ src/main/test/com/mainacad/dao/CartDAOTest.java
@@ -0,0 +1,123 @@
+package com.mainacad.dao;
+
+import com.mainacad.model.Cart;
+import com.mainacad.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartDAOTest {
+  private static List<Cart> carts = new ArrayList<>();
+  private static List<User> users = new ArrayList<>();
+  private static final Long NEW_CREATION_TIME = 1565024869119L;
+
+  @BeforeEach
+  void setUp() {
+    User user = new User("test_user_login", "test_pass", "test_name", "test_surname");
+    user = UserDAO.create(user);
+    users.add(user);
+
+    Cart cart = new Cart(1565024867119L, false, user.getId());
+    cart = CartDAO.create(cart);
+    carts.add(cart);
+  }
+
+  @AfterEach
+  void tearDown() {
+    for (Cart cart: carts) {
+      CartDAO.delete(cart.getId());
+    }
+    carts.clear();
+
+    for (User user: users) {
+      UserDAO.delete(user.getId());
+    }
+    users.clear();
+  }
+
+  @Test
+  void testCreate() {
+    Cart cart = new Cart(1565024867219L, false, users.get(0).getId());
+    cart = CartDAO.create(cart);
+    carts.add(cart);
+
+    assertNotNull(cart, "Creation method return null object");
+    assertNotNull(cart.getId(), "Object id is null. Creation method must update id field of Cart object.");
+
+    // checking findById
+    Cart checkedCart = CartDAO.findById(cart.getId());
+    assertNotNull(checkedCart, "Cart not found, but it should");
+  }
+
+  @Test
+  void testFind() {
+    Cart checkedCart = CartDAO.findById(carts.get(0).getId());
+    assertNotNull(checkedCart, "Cart not found, but it should");
+    assertEquals(carts.get(0).getId(), checkedCart.getId(), "Wrong cart found");
+  }
+
+  @Test
+  void testFindOpenCartByUser() {
+    Cart cart = carts.get(0);
+
+    Cart checkedCart = CartDAO.findOpenCartByUser(cart.getUserId());
+    assertNotNull(checkedCart, "findOpenCartByUser method return null object");
+
+    assertEquals(cart.getUserId(), checkedCart.getUserId());
+    assertEquals(cart.getClosed(), false);
+
+    checkedCart.setClosed(true);
+    CartDAO.update(checkedCart);
+
+    Cart checkedCartAfterUpdate = CartDAO.findOpenCartByUser(cart.getUserId());
+    if (checkedCartAfterUpdate != null) {
+      assertNotEquals(checkedCart.getId(), checkedCartAfterUpdate.getId());
+    }
+  }
+
+  @Test
+  void testFindByUser() {
+    Cart cart = carts.get(0);
+
+    List<Cart> checkedCartList = CartDAO.findByUser(cart.getUserId());
+    assertNotNull(checkedCartList, "findByUser method return null object");
+
+    Cart cartForCheck = null;
+    for (Cart cartFromCollection: checkedCartList) {
+      assertEquals(cart.getUserId(), cartFromCollection.getUserId());
+
+      if (cartFromCollection.getId() == cart.getId()) {
+
+        cartForCheck = cartFromCollection;
+      }
+    }
+    assertNotNull(cartForCheck, "Test cart not found, but it should");
+  }
+
+  @Test
+  void testUpdate() {
+    Cart cart = carts.get(0);
+
+    cart.setCreationTime(NEW_CREATION_TIME);
+    Cart checkedCart = CartDAO.update(cart);
+    assertNotNull(cart, "Update method return null object");
+    assertEquals(NEW_CREATION_TIME, checkedCart.getCreationTime());
+
+    checkedCart = CartDAO.findById(checkedCart.getId());
+    assertEquals(NEW_CREATION_TIME, checkedCart.getCreationTime());
+  }
+
+  @Test
+  void testDelete() {
+    Cart cart = carts.get(0);
+    CartDAO.delete(cart.getId());
+
+    Cart checkedCart = CartDAO.findById(cart.getId());
+    assertNull(checkedCart, "Cart not deleted from DB");
+  }
+}
